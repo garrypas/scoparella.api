f74202eeac3197b243b4ece6690c24ae02d10d5d
Garry Passarella <garry.passarella@tesco.com>
2020-08-19 23:41:03 +0100

    pipeline refinements

--- resources/modules/database/setup-database.sh
+++ resources/modules/database/setup-database.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-cat ../modules/database/setup-database.sql
+PASSWORD=$(az keyvault secret show --vault-name="${ENV}scoparellavault" --name=dbpassword -o tsv --query value)
+APP_PASSWORD=$(az keyvault secret show --vault-name="${ENV}scoparellavault" --name=dbapppassword -o tsv --query value)
 
-# PASSWORD=$(az keyvault secret show --vault-name="${ENV}scoparellavault" --name=dbpassword -o tsv --query value)
-# APP_PASSWORD=$(az keyvault secret show --vault-name="${ENV}scoparellavault" --name=dbapppassword -o tsv --query value)
-
-# if [ ! -z "${MODULE_PATH}" ]
-# then
-#   echo "MODULE_PATH SET TO $MODULE_PATH"
-# else
-#   MODULE_PATH="."
-# fi
-# echo "Insert password for placeholder..."
-# sed -i'' -e "s|<PASSWORD>|$APP_PASSWORD|g" "$MODULE_PATH/setup-database.sql"
-# sqlcmd -S "$ENV-scoparella-database-instance.database.windows.net" -d "master" -U scoparella_admin -P "$PASSWORD" -i "$MODULE_PATH/setup-database.sql"
-# echo "Put password placeholder back..."
-# sed -i'' -e "s|$APP_PASSWORD|<PASSWORD>|g" "$MODULE_PATH/setup-database.sql"
-# rm -rf ./*.sql-*
+if [ ! -z "${MODULE_PATH}" ]
+then
+  echo "MODULE_PATH SET TO $MODULE_PATH"
+else
+  MODULE_PATH="."
+fi
+echo "Insert password for placeholder..."
+sed -i'' -e "s|<PASSWORD>|$APP_PASSWORD|g" "$MODULE_PATH/setup-database.sql"
+sqlcmd -S "$ENV-scoparella-database-instance.database.windows.net" -d "master" -U scoparella_admin -P "$PASSWORD" -i "$MODULE_PATH/setup-database.sql"
+echo "Put password placeholder back..."
+sed -i'' -e "s|$APP_PASSWORD|<PASSWORD>|g" "$MODULE_PATH/setup-database.sql"
+rm -rf ./*.sql-*
