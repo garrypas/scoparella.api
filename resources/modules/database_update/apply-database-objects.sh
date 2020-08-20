f69ec03c666935e51030b894d4888b356bfc5979
Garry Passarella <garry.passarella@tesco.com>
2020-08-20 23:10:38 +0100

    Fixed database update bash

--- resources/modules/database_update/apply-database-objects.sh
+++ resources/modules/database_update/apply-database-objects.sh
@@ -9,6 +9,4 @@ else
   MODULE_PATH="."
 fi
 
-sed -i'' -e "s/\<PASSWORD\>/$APP_PASSWORD/g" $MODULE_PATH/database-objects.sql
-sqlcmd -S "$ENV-scoparella-database-instance.database.windows.net" -d "master" -U scoparella_admin -P "$PASSWORD" -i "$MODULE_PATH/database-objects.sql"
-sed -i'' -e "s/$APP_PASSWORD/\<PASSWORD\>/g" $MODULE_PATH/database-objects.sql
+sqlcmd -S "$ENV-scoparella-database-instance.database.windows.net" -d "scoparella" -U scoparella_admin -P "$PASSWORD" -i "$MODULE_PATH/database-objects.sql"
