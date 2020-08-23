69c83d480535949ed35a3ff4c2b79680221433a2
Garry Passarella <garry.passarella@tesco.com>
2020-08-24 00:57:07 +0100

    Fixed key generation

--- resources/modules/jwks/create-keys.sh
+++ resources/modules/jwks/create-keys.sh
@@ -1,15 +1,27 @@
-#!/bin/bash
-rm -rf ./RS512.key && rm -rf ./RS512.key.pub
-ssh-keygen -t rsa -b 4096 -m PEM -f ./RS512.key -Kqy -N ""
-openssl rsa -in ./RS512.key -pubout -outform PEM -out ./RS512.key.pub
+if [ -z "${ENV}" ]
+then
+  echo "ENV environment variable not specified. Exiting..."
+  exit 1
+fi
 
-public_key=`cat ./RS512.key.pub`
-private_key=`cat ./RS512.key`
+if [ ! -z "${MODULE_PATH}" ]
+then
+  echo "MODULE_PATH set to $MODULE_PATH"
+else
+  MODULE_PATH="."
+fi
+
+rm -rf $MODULE_PATH/RS512.key && rm -rf $MODULE_PATH/RS512.key.pub
+ssh-keygen -t rsa -b 4096 -m PEM -f $MODULE_PATH/RS512.key -Kqy -N ""
+openssl rsa -in $MODULE_PATH/RS512.key -pubout -outform PEM -out $MODULE_PATH/RS512.key.pub
+
+public_key=`cat $MODULE_PATH/RS512.key.pub`
+private_key=`cat $MODULE_PATH/RS512.key`
 
 az keyvault secret set --vault-name="${ENV}scoparellavault" --name=private-key --value="$private_key" -o none
 az keyvault secret set --vault-name="${ENV}scoparellavault" --name=public-key --value="$public_key" -o none
 
-rm -rf ./RS512.key.pub
-rm -rf ./RS512.key
+rm -rf $MODULE_PATH/RS512.key.pub
+rm -rf $MODULE_PATH/RS512.key
 
 echo "Setup keys"
